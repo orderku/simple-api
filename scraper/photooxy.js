999b9f53d38eeaa0542e18edf1944469789aa88e
akuwaee <96807908+akuwaee@users.noreply.github.com>
2022-01-05 10:19:09 +0700

    Update photooxy.js

--- scraper/photooxy.js
+++ scraper/photooxy.js
@@ -24,12 +24,12 @@ async function photoXy_3(URL, selectImage_0) {
 
 
 
-async function photoXy_1(URL, text[]) {
+async function photoXy_1(URL, text_1) {
 	return new Promise((resolve, reject) => {
 		axios.request({
 			url: URL,
 			method: "POST",
-			data: new URLSearchParams(Object.entries({ text[]: text[], login: "OK"})),
+			data: new URLSearchParams(Object.entries({ text_1: text_1, login: "OK"})),
 			headers: {
 				"content-type": "application/x-www-form-urlencoded",
 				"user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"
