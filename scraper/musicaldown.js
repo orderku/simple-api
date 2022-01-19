9dedce7871e45d06aaef4938d2dd6ba1363ad1be
akuwaee <96807908+akuwaee@users.noreply.github.com>
2022-01-19 19:25:16 +0700

    Update musicaldown.js

--- scraper/musicaldown.js
+++ scraper/musicaldown.js
@@ -1,57 +1,45 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-
-async function musicaldown(URL) {
-    return new Promise((resolve, rejecet) => {
-        axios.get('https://musicaldown.com/id', {
-            headers: {
-                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
-            }
-        }).then(res => {
-            const $ = cheerio.load(res.data)
-            const url_name = $("#link_url").attr("name")
-            const token_name = $("#submit-form > div").find("div:nth-child(1) > input[type=hidden]:nth-child(2)").attr("name")
-            const token_ = $("#submit-form > div").find("div:nth-child(1) > input[type=hidden]:nth-child(2)").attr("value")
-            const verify = $("#submit-form > div").find("div:nth-child(1) > input[type=hidden]:nth-child(3)").attr("value")
-            let data = {
-                [`${url_name}`]: URL,
-                [`${token_name}`]: token_,
-                verify: verify
-            }
-        axios.request({
-            url: 'https://musicaldown.com/id/download',
-            method: 'post',
-            data: new URLSearchParams(Object.entries(data)),
-            headers: {
-                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
-                'cookie': res.headers["set-cookie"]
-            }
-        }).then(respon => {
-            const ch = cheerio.load(respon.data)
-        axios.request({
-            url: 'https://musicaldown.com/id/mp3',
-            method: 'post',
-            headers: {
-                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
-                'cookie': res.headers["set-cookie"]
-            }
-        }).then(resaudio => { 
-            const hc = cheerio.load(resaudio.data)       
-            const result = {
-                creator: 'Arya-kun >///<',
-                video: ch('body > div.welcome.section > div> div').find('div:nth-child(2) > div.col.s12.l8 > a:nth-child(4)').attr('href'),
-                audio: hc('body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(6)').attr('href'),
-                nowm: ch('body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(3)').attr('href'),
-                video_original: ch('body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(8)').attr('href'),
-                audio_original: hc('body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(8)').attr('href'),
-                preview: ch('body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l4 > img').attr('src')
-            }
-        resolve(result)
-        })
-    })
-    })
-    })
+async function musicaldown (Url) {
+	return new Promise (async (resolve, reject) => {
+		await axios.request({
+			url: "https://ttdownloader.com/",
+			method: "GET",
+			headers: {
+				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+				"accept-language": "en-US,en;q=0.9,id;q=0.8",
+				"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+				"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
+			}
+		}).then(respon => {
+			const $ = cheerio.load(respon.data)
+			const token = $('#token').attr('value')
+			axios({
+				url: "https://ttdownloader.com/req/",
+				method: "POST",
+				data: new URLSearchParams(Object.entries({url: Url, format: "", token: token})),
+				headers: {
+					"accept": "*/*",
+					"accept-language": "en-US,en;q=0.9,id;q=0.8",
+					"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+					"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+					"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
+				}
+			}).then(res => {
+				const ch = cheerio.load(res.data)
+				const result = {
+					status: res.status,
+					result: {
+						nowm: ch('#results-list > div:nth-child(2)').find('div.download > a').attr('href'),
+						video_original: ch('#results-list > div:nth-child(3)').find('div.download > a').attr('href'),
+						audio: ch('#results-list > div:nth-child(4)').find(' div.download > a').attr('href')
+					}
+				}
+				resolve(result)
+			}).catch(reject)
+		}).catch(reject)
+	})
 }
 
 module.exports =  { musicaldown }
